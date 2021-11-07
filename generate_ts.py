b0bb273a54418fe939a7b6149359b91d17c5fde9
William Goodwin <gwill8@vt.edu>
2021-11-06 22:41:52 -0400

    added CliArguments class to hold the input and output file paths

--- generate_ts.py
+++ generate_ts.py
@@ -1,5 +1,10 @@
 import argparse
 
+class CliArguments:
+    def __init__(self, input_file_path, output_file_path):
+        self.input_file_path = input_file_path
+        self.output_file_path = output_file_path
+
 def fail_ts_generation(failure_message):
     print("TS generation unsuccessful:")
     print("     " + failure_message)
@@ -14,8 +19,7 @@ def read_file():
 def generate_ts():
     print("hello")
 
-def verify_input_file():
-    print("gql or graphql (or json?)")
+def verify_input_file(input_file_path: str):
 
 def parse_cli_args():
     parser = argparse.ArgumentParser()
@@ -27,10 +31,10 @@ def parse_cli_args():
     if not args.OutputFile or not args.InputFile:
         fail_ts_generation("Input and output file args required")
 
-    return args
+    return CliArguments(args.InputFile, args.OutputFile)
 
 
 if __name__ == "__main__":
-    parse_cli_args()
-    verify_input_file()
+    args = parse_cli_args()
+    verify_input_file(args.input_file_path)
     generate_ts()
