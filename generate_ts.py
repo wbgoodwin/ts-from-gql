75156ee1eb77c2d8a1b0ccd2c0905231f5b09080
William Goodwin <gwill8@vt.edu>
2021-11-07 11:32:54 -0500

    gql types are written as interfaces to file

--- generate_ts.py
+++ generate_ts.py
@@ -11,14 +11,78 @@ def fail_ts_generation(failure_message):
     print("     " + failure_message)
     exit(1)
 
-def write_to_file():
-    print()
+def write_to_file(output_file_path, lines_to_write):
+    with open("output_file_path", "w") as file:
+        file.writelines(lines_to_write)
 
-def read_file():
-    print()
 
-def generate_ts():
-    print("hello")
+def read_file(input_file_path):
+    file_lines: list[str]
+    with open(input_file_path) as file:
+        file_lines = file.readlines()
+    return file_lines
+
+def line_indicates_query_or_mutation(split_line: list[str]):
+    return len(split_line) == 3 and (split_line[1] == "Query" or split_line[1] == "Mutation") and "{" in split_line[2]
+
+
+def remove_queries_and_mutations(lines: list[str]):
+    updated_lines: list[str] = list()
+    in_query_or_mutation = False
+    for line in lines:
+        if in_query_or_mutation and "}" in line:
+            in_query_or_mutation = False
+        elif not in_query_or_mutation:
+            whitespace_split_str = line.split()
+            if line_indicates_query_or_mutation(whitespace_split_str):
+                in_query_or_mutation = True
+            else:
+                updated_lines.append(line)
+    return updated_lines
+
+
+
+def convert_type(input_type: str):
+    # would prefer to use match-case, but that is new and want this to be
+    # compatible with older versions of python.
+    parsed_type = input_type.strip().split(" ")[0].replace("!", "")
+    if "String" in parsed_type:
+        return "string"
+    elif "Int" in parsed_type:
+        return "number"
+    elif "UUID" in parsed_type:
+        return "Uuid"
+    else:
+        return parsed_type
+
+
+def convert_line(line: str):
+    if line.lstrip().startswith("#"):
+        return
+
+    whitespace_split_str = line.split()
+    if len(whitespace_split_str) == 3 and whitespace_split_str[0] == "type" and "{" in whitespace_split_str[2]:
+        return line.replace("type", "interface")
+
+    colon_split_str = line.split(":")
+    if len(colon_split_str) == 2:
+        return line.replace(
+                colon_split_str[1],
+                " " + convert_type(colon_split_str[1]) + "\n"
+            )
+
+    return line
+    
+
+def generate_ts(args: CliArguments):
+    input_file_lines = read_file(args.input_file_path)
+    output_file_lines: list[str] = list()
+    for line in input_file_lines:
+        converted_line = convert_line(line)
+        if converted_line:
+            output_file_lines.append(converted_line)
+    output_file_lines = remove_queries_and_mutations(output_file_lines)
+    write_to_file(args.output_file_path, output_file_lines)
 
 def verify_input_file(input_file_path: str):
     if not input_file_path.endswith(".gql") and not input_file_path.endswith(".graphql"):
@@ -42,4 +106,12 @@ def parse_cli_args():
 if __name__ == "__main__":
     args = parse_cli_args()
     verify_input_file(args.input_file_path)
-    generate_ts()
+    generate_ts(args)
+    print("TS file generation complete.")
+
+
+# Want to remove comment lines
+# Remove !
+# remove @deprecated?
+# only convert type -> interface if it is referencing an actual gql type, not just the word happens to be there.
+# String -> string
